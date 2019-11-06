0e975f3b4f6fa1eab4744e3e2f8fb3714a1a8824
Samurrai19 <57445941+Samurrai19@users.noreply.github.com>
2019-11-06 17:09:08 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href="/styles/styles.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap-4.3.1-dist/js/bootstrap.min.js">
+	<link rel="shortcut icon" href="application/img/favicon.png" type="image/png">
+	<title>Never give up</title>
+</head>
+<body>
+	<div id="particles-js"></div>
+	<div class="bg-image"></div>
+	<div class="page-wrapper"><h1><div class="main-box" ><div class="main-logo"><img src="application/img/Logo.png"></div> NEVER GIVE UP<br>НИКОГДА НЕ СДАВАЙСЯ
+	<div class="awe-icons"> 
+	<a href="https://vk.com/samurrai19" class="icon-vk" target="_blank"><i class="fab fa-vk"></i></a>
+	<a href="https://www.instagram.com/samurrai19" class="icon-inst" target="_blank"><i class="fab fa-instagram"></i></a>
+	</div>
+	</div></h1></div>
+	<script src="https://kit.fontawesome.com/fd7139e392.js" crossorigin="anonymous" defer></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" defer></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js" defer></script>
+	<script src="js/main.js" defer></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" defer ></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" defer ></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" defer ></script>
+</body>
---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', 'js/assets/particles/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+	});
+	
+});
